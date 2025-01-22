aa5afdb59d7dabb3f0c6f25c56d6f90de0da0902
dallasplunkett <dallasplunkett@gmail.com>
2025-01-22 10:38:32 -0800

    completed step 3

--- global.js
+++ global.js
@@ -3,3 +3,55 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
+
+// Step 2
+// let navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+// );
+
+// if (currentLink) {
+//     currentLink.classList.add('current');
+// }
+
+// Step 3
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'cv/', title: 'CV' },
+    { url: 'https://github.com/dallasplunkett', title: 'GitHub' },
+];
+
+let nav = document.createElement('nav');
+document.querySelector(".container").prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+        url = '../' + url;
+    }
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+
+    if (a.host !== location.host) {
+        a.target = '_blank';
+        a.rel = 'noopener noreferrer';
+    }
+
+    nav.appendChild(a)
+}
+
+// workflow: create-modify-append
